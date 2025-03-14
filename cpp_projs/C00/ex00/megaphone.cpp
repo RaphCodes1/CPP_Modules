f162b244a6778dec2f80b5d99774e07e7141cc99
Raph <143648409+RaphCodes1@users.noreply.github.com>
2025-03-14 23:45:51 +0000

    added ex00

--- cpp_projs/C00/ex00/megaphone.cpp
+++ cpp_projs/C00/ex00/megaphone.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(int ac, char **av)
+{   
+    if(ac == 1)
+        cout << "* LOUD AND UNBREARABLE FEEDBACK NOISE *\n";
+    else
+    {   
+        for(int i = 1; i < ac; i++)
+        {   
+            for(int f = 0;f < av[i][f] != '\0'; f++)
+                cout << static_cast<char>(toupper(av[i][f]));
+        }
+        cout <<endl;
+    }
     return (0);
 }
