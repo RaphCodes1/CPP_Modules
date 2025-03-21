9382699c6d7bac64ec1ab570a848ce6cac85d613
RaphCodes1 <raphaelcreer@gmail.com>
2025-03-22 00:12:43 +0400

    bruh

--- cpp_projs/C01/ex01/zombieHorde.cpp
+++ cpp_projs/C01/ex01/zombieHorde.cpp
@@ -8,7 +8,7 @@ Zombie *zombieHorde(int N, std::string name)
     Zombie* horde = new Zombie[N];
 
     for(int i = 0;i < N; i++)
-        horde[i].setName(name + "_" + std::to_string(i + 1));\
+        horde[i].setName(name + "_" + std::to_string(i + 1));
     
     return (horde);
 };
