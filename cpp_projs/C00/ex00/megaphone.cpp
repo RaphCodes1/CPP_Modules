b643166ed618c2b9b64ec74b1ad56593170d16ed
Raphael Creer <rcreer@lab1r6s4.42abudhabi.ae>
2025-04-21 18:10:19 +0400

    updated C00

--- cpp_projs/C00/ex00/megaphone.cpp
+++ cpp_projs/C00/ex00/megaphone.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 #include <string>
-#include <cctype>
-using namespace std;
 
 int main(int ac, char **av)
 {   
     if(ac == 1)
-        cout << "* LOUD AND UNBREARABLE FEEDBACK NOISE *\n";
+        std::cout << "* LOUD AND UNBREARABLE FEEDBACK NOISE *\n";
     else
     {   
         for(int i = 1; i < ac; i++)
         {   
             for(int f = 0;f < av[i][f] != '\0'; f++)
-                cout << static_cast<char>(toupper(av[i][f]));
+                std::cout << char(toupper(av[i][f]));
         }
-        cout <<endl;
+        std::cout << std::endl;
     }
     return (0);
 }
