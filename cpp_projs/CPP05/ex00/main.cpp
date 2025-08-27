946497dac940fb3ae8bc7688b91912d9e470f23a
RaphCodes1 <raphaelcreer@gmail.com>
2025-08-27 18:53:24 +0400

    changed main ex00

--- cpp_projs/CPP05/ex00/main.cpp
+++ cpp_projs/CPP05/ex00/main.cpp
@@ -3,21 +3,20 @@
 int main()
 {
     try{
-        Bureaucrat a("Mike",1);
-    } catch (const Bureaucrat::GradeTooHighException& e) {
-        std::cerr << "caught an exception: " << e.what() << std::endl;
-    } catch (const Bureaucrat::GradeTooLowException& e) {
+        Bureaucrat a("Mike",0);
+    } catch (const std::exception& e) {
         std::cerr << "caught an exception: " << e.what() << std::endl;
     }
 
     Bureaucrat b("Kyle",10);
 
+    Bureaucrat c(b);
     try{
         b.increaseGrade(9);
+        c.reduceGrade(3);
         std::cout << b << std::endl;
-    } catch(const Bureaucrat::GradeTooHighException& e){
-        std::cerr << "caught exception: " << e.what() << std::endl;
-    } catch(const Bureaucrat::GradeTooLowException& e){
+        std::cout << c << std::endl;
+    } catch(const std::exception& e){
         std::cerr << "caught exception: " << e.what() << std::endl;
     }
 }
