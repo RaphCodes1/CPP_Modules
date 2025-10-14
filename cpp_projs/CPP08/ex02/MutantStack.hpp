0a6de8466906a17678e02380355568befd705b28
Raphael Creer <rcreer@lab1r5s6.42abudhabi.ae>
2025-10-14 17:46:48 +0400

    complete ex02

--- cpp_projs/CPP08/ex02/MutantStack.hpp
+++ cpp_projs/CPP08/ex02/MutantStack.hpp
@@ -12,9 +12,24 @@ class MutantStack : public std::stack<T>{
         ~MutantStack();
         MutantStack(const MutantStack<T> &src);
         MutantStack &operator=(const MutantStack<T> &src);
+
         typedef typename std::deque<T>::iterator iterator;
+        typedef typename std::deque<T>::const_iterator const_iterator;
+        typedef typename std::deque<T>::reverse_iterator reverse_iterator;
+        typedef typename std::deque<T>::const_reverse_iterator const_reverse_iterator;
+
         iterator begin();
         iterator end();
+
+        const_iterator begin() const;
+        const_iterator end() const;
+
+        reverse_iterator rbegin();
+        reverse_iterator rend();
+
+        const_reverse_iterator rbegin() const;
+        const_reverse_iterator rend() const;
+        
 };
 
 #include "MutantStack.tpp"
