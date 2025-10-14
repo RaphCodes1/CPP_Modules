e9b267508a2e92a01fa29a7d776facc600bbc5be
Raphael Creer <rcreer@lab1r5s6.42abudhabi.ae>
2025-10-14 16:41:24 +0400

    keep things simple

--- cpp_projs/CPP08/ex01/main.cpp
+++ cpp_projs/CPP08/ex01/main.cpp
@@ -7,20 +7,9 @@ int main()
     sp.addNumber(17);
     sp.addNumber(9);
     sp.addNumber(11);
+    
     std::cout << sp.shortestSpan() << std::endl;
     std::cout << sp.longestSpan() << std::endl;
 
-    srand(time(0));
-    try{
-        Span sp = Span(10000);
-        for(int i = 0; i < sp.getSize();i++)
-        {
-            sp.addNumber(i);
-        }
-        std::cout << "Random shortest: " << sp.shortestSpan() << std::endl;
-        std::cout << "Random longest: " << sp.longestSpan() << std::endl;
-    } catch(std::exception &e){
-        std::cout << "exception found: " << e.what() << std::endl;
-    }
     return 0;
 }
