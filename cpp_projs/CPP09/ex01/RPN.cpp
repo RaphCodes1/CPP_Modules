719438b182ecbe93212a3f206e9d6a72ce102863
Raphael Creer <rcreer@lab1r4s7.42abudhabi.ae>
2025-10-20 19:13:28 +0400

    work in progress

--- cpp_projs/CPP09/ex01/RPN.cpp
+++ cpp_projs/CPP09/ex01/RPN.cpp
@@ -19,6 +19,12 @@ RPN &RPN::operator=(const RPN& src){
 void RPN::RPNprocess(std::string s){
     
     std::cout << s << std::endl;
+
+    int num1;
+    int num2;
+    char operation;
+
+    // for(size_t i = 0; i < s.length())
     
 
 }
